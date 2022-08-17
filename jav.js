98ab3f8c96ca19a6545ca519adadd2739867a55d
Carissa Hibbert <108243658+carhibbert@users.noreply.github.com>
2022-08-16 22:57:20 -0500

    Add files via upload

---
+++ jav.js
@@ -0,0 +1,26 @@
+function getRandomNumber() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(Math.random())
+        }, 500)
+    })
+}
+
+async function printRandomNumber() {
+    const randomNumber = await getRandomNumber()
+    console.log(randomNumber)
+}
+
+printRandomNumber()
+
+async function getCityData(city) {
+    const AUTH_KEY ='186000864844051202681x117219'
+    const url = `https://geocode.xyz/${city}?json=1&auth=${AUTH_KEY}`
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data.latt, data.longt)
+}
+function init() {
+    const cityInput = document.getElementById("nameInput").value;
+    cityInput.onclick=getCityData(cityInput);
+}
